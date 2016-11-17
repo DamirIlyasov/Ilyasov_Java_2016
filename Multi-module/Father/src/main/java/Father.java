50d4fa47c7ca8e26df2f16aed142084552614ea9
Damir <damirilyasov1997@gmail.com>
2016-11-17 18:41:32 +0300

    [AY-5]:Multi-Module project added.

---
+++ Multi-module/Father/src/main/java/Father.java
@@ -0,0 +1,7 @@
+/**
+ * Created by damir on 17.11.16.
+ */
+public class Father {
+    static String lastName = "Ilyasov";
+    static String firsName = "Damir";
+}
---
+++ Multi-module/Mother/src/main/java/Mother.java
@@ -0,0 +1,8 @@
+/**
+ * Created by damir on 17.11.16.
+ */
+public class Mother {
+    String firstName = "someName";
+    String lastName = Father.lastName;
+    static String eyeColor = "blue";
+}
