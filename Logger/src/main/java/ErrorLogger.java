124fb7ca7f379891fa5998400d07eca0adcc7a3b
Damir <damirilyasov1997@gmail.com>
2016-11-17 19:27:08 +0300

    [AY-8]: Logger added.

---
+++ Logger/src/main/java/ErrorLogger.java
@@ -0,0 +1,14 @@
+public class ErrorLogger extends Logger{
+    @Override
+    public void log(String message) {
+        String[] s = message.split(" : ");
+        level = s[0];
+        if (level.equals("[ERROR]")) {
+            String text = s[1];
+            writeMessage(text);
+        }
+        if (next != null) {
+            next.log(message);
+        }
+    }
+}
---
+++ Logger/src/main/java/Logger.java
@@ -0,0 +1,14 @@
+abstract public class Logger {
+    protected String level;
+    protected Logger next;
+
+    public void setNext(Logger next) {
+        this.next = next;
+    }
+
+    public abstract void log(String message);
+
+    void writeMessage(String message){
+        System.out.println(message);
+    }
+}
