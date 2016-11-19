f92810946a5062c2059a56c5f4d2151fe80205c3
Damir <damirilyasov1997@gmail.com>
2016-11-19 15:43:06 +0300

    [AY-5]:Multi-midule project added.

---
+++ Module/B/src/main/java/TestClass.java
@@ -0,0 +1,4 @@
+/**
+ * Created by damir on 15.11.16.
+ */
+
---
+++ Module/C/src/main/java/ClassFromC.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by damir on 18.11.16.
+ */
+public class ClassFromC {
+    public static List a = new LinkedList();
+
+}
